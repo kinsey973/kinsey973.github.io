fc8d2732f94cbf1610bf6fec52d2f66b8e206023
kinsey973 <kinsey973@users.noreply.github.com>
2025-08-11 11:28:25 +0000

    deploy: 1e84516d8291f22086b04deff5b60f2f23a27a96

---
+++ assets/back.JsICmOsg.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunks/vue-router.C9mbL1_w.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
